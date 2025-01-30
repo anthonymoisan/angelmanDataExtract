24f254909b81b2f207a8ddd05233885905465e88
anthonymoisan <anthonymoisan@yahoo.fr>
2025-01-30 15:47:01 +0100

    clean src

--- src/appFlaskMySQL.py
+++ src/appFlaskMySQL.py
@@ -4,7 +4,11 @@ import os
 from configparser import ConfigParser
 import sshtunnel
 from sshtunnel import SSHTunnelForwarder
+import pandas as pd
+import time
 
+#Very important parameter to execute locally or remotely (production)
+LOCAL_CONNEXION = True
 
 appFlaskMySQL = Flask(__name__)
 appFlaskMySQL.config["DEBUG"] = True
@@ -25,24 +29,33 @@ SSH_PASSWORD = config['SSH']['SSH_PASSWORD']
 sshtunnel.SSH_TIMEOUT = 10.0
 sshtunnel.TUNNEL_TIMEOUT = 10.0
 
-LOCAL_CONNEXION = True 
-
-def selectSQL(DATABASE_URL,request):
+def readTable(DATABASE_URL,tableName):
     try:
         engine = create_engine(DATABASE_URL)        
         with engine.connect() as connection:
-            result = connection.execute(text(request))
-            recipes = [dict(row) for row in result.mappings()]
-            return jsonify(recipes)  
+            df = pd.read_sql_table(tableName, connection)
+            df.fillna("None",inplace = True)    
+            dict_df = df.to_dict(orient='records') 
+            return jsonify(dict_df)  
     except Exception as e:
          print("Connexion error :", e)
     finally:
         engine.dispose()
       
+@appFlaskMySQL.route('/', methods=['GET'])
+def home():
+    return '''<h1>APIs</h1>
+<ul>    
+<li>API in order for scraping data from PubMed : <a href="./api/v1/resources/articlesPubMed">./api/v1/resources/articlesPubMed</a></li>
+<li>API in order for scraping data from AS Trial : <a href="./api/v1/resources/ASTrials">./api/v1/resources/ASTrials</a></li>
+<li>API in order for scraping data from UN Population : <a href="./api/v1/resources/UnPopulation">./api/v1/resources/UnPopulation</a></li>
+<li>API in order for scraping data from ASF Clinical Trials : <a href="./api/v1/resources/ASFClinicalTrials">./api/v1/resources/ASFClinicalTrials</a></li>
+</ul>
+'''
 
-
-@appFlaskMySQL.route('/recettes', methods=['GET'])
-def get_recipes():
+@appFlaskMySQL.route('/api/v1/resources/ASTrials', methods=['GET'])
+def api_ASTrials_all():
+    start = time.time()
     try:
         if(LOCAL_CONNEXION):
             # Création du tunnel SSH
@@ -54,10 +67,77 @@ def get_recipes():
             ) as tunnel:
                     local_port = tunnel.local_bind_port
                     DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@127.0.0.1:{local_port}/{DB_NAME}"
-                    return selectSQL(DATABASE_URL,"SELECT * FROM recette")
+                    return readTable(DATABASE_URL,"T_ASTrials")
         else:
             DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-            return selectSQL(DATABASE_URL,"SELECT * FROM recette")
+            return readTable(DATABASE_URL,"T_ASTrials")
+        print("Execute time for ASTrials : ",round(time.time() - start,2), "s")
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@appFlaskMySQL.route('/api/v1/resources/articlesPubMed', methods=['GET'])
+def api_articles_all():
+    start = time.time()
+    try:
+        if(LOCAL_CONNEXION):
+            # Création du tunnel SSH
+            with SSHTunnelForwarder(
+                (SSH_HOST),
+                ssh_username=SSH_USERNAME,
+                ssh_password=SSH_PASSWORD,
+                remote_bind_address=(DB_HOST, 3306)
+            ) as tunnel:
+                    local_port = tunnel.local_bind_port
+                    DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@127.0.0.1:{local_port}/{DB_NAME}"
+                    return readTable(DATABASE_URL,"T_ArticlesPubMed")
+        else:
+            DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+            return readTable(DATABASE_URL,"T_ArticlesPubMed")
+        print("Execute time for articlesPubMed : ",round(time.time() - start,2), "s")
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@appFlaskMySQL.route('/api/v1/resources/UnPopulation', methods=['GET'])
+def api_UnPopulation_all():
+    start = time.time()
+    try:
+        if(LOCAL_CONNEXION):
+            # Création du tunnel SSH
+            with SSHTunnelForwarder(
+                (SSH_HOST),
+                ssh_username=SSH_USERNAME,
+                ssh_password=SSH_PASSWORD,
+                remote_bind_address=(DB_HOST, 3306)
+            ) as tunnel:
+                    local_port = tunnel.local_bind_port
+                    DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@127.0.0.1:{local_port}/{DB_NAME}"
+                    return readTable(DATABASE_URL,"T_UnPopulation")
+        else:
+            DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+            return readTable(DATABASE_URL,"T_UnPopulation")
+            print("Execute time for UnPopulation : ",round(time.time() - start,2), "s")
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@appFlaskMySQL.route('/api/v1/resources/ASFClinicalTrials', methods=['GET'])
+def api_ASFClinicaltrials_all():
+    start = time.time()
+    try:
+        if(LOCAL_CONNEXION):
+            # Création du tunnel SSH
+            with SSHTunnelForwarder(
+                (SSH_HOST),
+                ssh_username=SSH_USERNAME,
+                ssh_password=SSH_PASSWORD,
+                remote_bind_address=(DB_HOST, 3306)
+            ) as tunnel:
+                    local_port = tunnel.local_bind_port
+                    DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@127.0.0.1:{local_port}/{DB_NAME}"
+                    return readTable(DATABASE_URL,"T_ASFClinicalTrials")
+        else:
+            DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+            return readTable(DATABASE_URL,"T_ASFClinicalTrials")
+        print("Execute time for ASFClinicalTrials : ",round(time.time() - start,2), "s") 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
--- src/scraper/__init__.py
+++
@@ -1,4 +0,0 @@
-from .scraperPubMed import pubmed_by_year
-from .scraperASFClinicalTrial import trials_asf_clinics
-from .scraperASTrial import as_trials
-from .scraperPopulation import un_population
