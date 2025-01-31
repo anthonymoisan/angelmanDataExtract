7fc661062ac86de607faefa70d9ad86e64a31ccf
anthonymoisan <anthonymoisan@yahoo.fr>
2025-01-31 14:33:18 +0100

    Documentation of databaseSQL

--- src/databaseSQL.py
+++ src/databaseSQL.py
@@ -12,7 +12,7 @@ import pandas as pd
 import numpy as np
 import json
 
-#Very important parameter to execute locally or remotely (production)
+# Very important parameter to execute locally or remotely (production)
 LOCAL_CONNEXION = True
 
 # Read SSH information and DB information in a file Config2.ini
@@ -34,14 +34,22 @@ else:
 sshtunnel.SSH_TIMEOUT = 100.0
 sshtunnel.TUNNEL_TIMEOUT = 100.0
 
-def tryExecuteRequest(DATABASE_URL,request):
+
+def __tryExecuteRequest(DATABASE_URL, request):
+    """
+    Try execute a request SQL with the SQL request
+
+    Arguments:
+    DATABASE_URL : url of the database
+    request – SQL request
+    """
     # Create engine SQLAlchemy
     engine = create_engine(DATABASE_URL)
 
     # Try the connexion
     try:
         with engine.connect() as connection:
-            print("Connect to the database via SSH Tunnel !")
+            print("Connect to the database !")
 
             # Execute the request SQL
             result = connection.execute(text(request))
@@ -51,8 +59,14 @@ def tryExecuteRequest(DATABASE_URL,request):
         engine.dispose()
         print("Connexion closed.")
 
-def execRequest(request):
-    '''Execute a request SQL with the parameter request in local or remotely'''
+
+def __execRequest(request):
+    """
+    Execute a request SQL with the parameter request in local or remotely 
+
+    Arguments:
+    request – SQL request
+    """
     
     if LOCAL_CONNEXION:
         # Create SSH Tunnel
@@ -63,32 +77,50 @@ def execRequest(request):
             remote_bind_address=(DB_HOST, 3306)
         ) as tunnel:
 
-            # # ***/ """
+            # Local connexion
             local_port = tunnel.local_bind_port
-            DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@127.0.0.1:{local_port}/{DB_NAME}"
-            tryExecuteRequest(DATABASE_URL,request) 
-    else:
-        #remotely
-        DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-        tryExecuteRequest(DATABASE_URL,request)
+            DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@127.0.0.1: {local_port}/{DB_NAME}"
 
-def tryInsertValue(DATABASE_URL,tableName,df):
-    # # Create engine SQLAlchemy
+            __tryExecuteRequest(DATABASE_URL, request)
+    else:
+        # Remote connexion
+        DATABASE_URL = f"""mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"""
+
+        __tryExecuteRequest(DATABASE_URL, request)
+
+
+def __tryInsertValue(DATABASE_URL, tableName, df):
+    """
+    Try insert Value from df in tableName with Database_URL 
+
+    Arguments:
+    DATABASE_URL – URL of the database
+    df – dataframe
+    tableName – tableName from database
+    """
+    # Create engine SQLAlchemy
     engine = create_engine(DATABASE_URL)
 
     # Try connexion
     try:
         with engine.connect() as connection:
             df.to_sql(tableName, con=connection, if_exists='replace', index=False)
-            print("Insert values in ",tableName)
+            print("Insert values in ", tableName)
     except Exception as e:
         print("Connexion error :", e)
     finally:
         engine.dispose()
         print("Connexion closed.")
 
-def insertValue(df,tableName):
-    
+
+def __insertValue(df, tableName):
+    """
+    Insert Value from df in tableName 
+
+    Arguments:
+    df – dataframe
+    tableName – tableName from database
+    """
     if LOCAL_CONNEXION:
         # Création du tunnel SSH
         with SSHTunnelForwarder(
@@ -101,118 +133,138 @@ def insertValue(df,tableName):
             # # ***/ """
             local_port = tunnel.local_bind_port
             DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@127.0.0.1:{local_port}/{DB_NAME}"
-            tryInsertValue(DATABASE_URL,tableName,df)
+
+            __tryInsertValue(DATABASE_URL, tableName, df)
     else:
-        #remotely
-        DATABASE_URL = f"mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-        tryInsertValue(DATABASE_URL,tableName,df)
-    
+        # Remotely
+        DATABASE_URL = f"""mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"""
+
+        __tryInsertValue(DATABASE_URL, tableName, df)
+
+
 def asTrials():
+    """
+    Method to scrap data from as clinical trials
+    """
     start = time.time()
     wkdir = os.path.dirname(__file__)
-    #0) Drop Table
+    # 0) Drop Table
     print("--- Drop Table")
     sqlDrop = "DROP TABLE T_ASTrials"
-    execRequest(sqlDrop)
+    __execRequest(sqlDrop)
 
-    #1) Create Table
+    # 1) Create Table
     print("--- Create Table")
     with open(f"{wkdir}/SQLScript/createASTrials.sql", "r", encoding="utf-8") as file:
         sql_commands = file.read()
-    execRequest(sql_commands)
-    
-    print("--- Scraper")
-    #2) Use scraper to obtain dataframe
-    df = scrASTrial.as_trials()
-    df  = df.replace([np.inf, -np.inf], np.nan)
-    df.fillna("None",inplace = True)
+    __execRequest(sql_commands)
 
-    #3) Insert value in Table from dataframe
-    insertValue(df,"T_ASTrials")
-    print("Execute time for ASTrials : ",round(time.time() - start,2), "s")
+    print("--- Scraper")
+    # 2) Use scraper to obtain dataframe
+    df = scrASTrial.as_trials()
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.fillna("None", inplace=True)
+
+    # 3) Insert value in Table from dataframe
+    __insertValue(df, "T_ASTrials")
+    print("Execute time for ASTrials : ", round(time.time()-start, 2), "s")
+
 
 def articlesPubMed():
+    """
+    Method to scrap data from as Pub Med
+    """
     start = time.time()
     wkdir = os.path.dirname(__file__)
-    #0) Drop Table
+    # 0) Drop Table
     print("--- Drop Table")
     sqlDrop = "DROP TABLE T_ArticlesPubMed"
-    execRequest(sqlDrop)
+    __execRequest(sqlDrop)
 
-    #1) Create Table
+    # 1) Create Table
     print("--- Create Table")
     with open(f"{wkdir}/SQLScript/createArticlesPubMed.sql", "r", encoding="utf-8") as file:
         sql_commands = file.read()
-    execRequest(sql_commands)
-    
-    print("--- Scraper")
-    #2) Use scraper to obtain dataframe
-    df = scrPubMed.pubmed_by_year(1965)
-    df  = df.replace([np.inf, -np.inf], np.nan)
-    df.fillna("None",inplace = True)
+    __execRequest(sql_commands)
 
-    #3) Insert value in Table from dataframe
-    insertValue(df,"T_ArticlesPubMed")
-    print("Execute time for articlesPubMed : ",round(time.time() - start,2), "s")
+    print("--- Scraper")
+    # 2) Use scraper to obtain dataframe
+    df = scrPubMed.pubmed_by_year(1965)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.fillna("None", inplace=True)
+
+    # 3) Insert value in Table from dataframe
+    __insertValue(df, "T_ArticlesPubMed")
+    print("Execute time for articlesPubMed : ", round(time.time()-start, 2), "s")
+
 
 def unPopulation():
+    """
+    Method to scrap data from Un Population
+    """
     start = time.time()
     wkdir = os.path.dirname(__file__)
-    #0) Drop Table
+    # 0) Drop Table
     print("--- Drop Table")
     sqlDrop = "DROP TABLE T_UnPopulation"
-    execRequest(sqlDrop)
+    __execRequest(sqlDrop)
 
-    #1) Create Table
+    # 1) Create Table
     print("--- Create Table")
     with open(f"{wkdir}/SQLScript/createUnPopulation.sql", "r", encoding="utf-8") as file:
         sql_commands = file.read()
-    execRequest(sql_commands)
-    
+    __execRequest(sql_commands)
+
     print("--- Scraper")
-    #2) Use scraper to obtain dataframe
+    # 2) Use scraper to obtain dataframe
     config = ConfigParser()
     filePath = f"{wkdir}/../angelman_viz_keys/Config3.ini"
     if config.read(filePath):
         auth_token = config['UnPopulation']['bearerToken']
-    df =  scrPopulation.un_population(auth_token)
-    df  = df.replace([np.inf, -np.inf], np.nan)
-    df.fillna("None",inplace = True)
+    df = scrPopulation.un_population(auth_token)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.fillna("None", inplace=True)
 
-    #3) Insert value in Table from dataframe
-    insertValue(df,"T_UnPopulation")
-    print("Execute time for UnPopulation : ",round(time.time() - start,2), "s")
+    # 3) Insert value in Table from dataframe
+    __insertValue(df, "T_UnPopulation")
+    print("Execute time for UnPopulation : ", round(time.time()-start, 2), "s")
+
 
 def asfClinicalTrials():
+    """
+    Method to scrap data from as clinical trials and ASF files
+    """
     start = time.time()
     wkdir = os.path.dirname(__file__)
-    #0) Drop Table
+    # 0) Drop Table
     print("--- Drop Table")
     sqlDrop = "DROP TABLE T_ASFClinicalTrials"
-    execRequest(sqlDrop)
+    __execRequest(sqlDrop)
 
-    #1) Create Table
+    # 1) Create Table
     print("--- Create Table")
     with open(f"{wkdir}/SQLScript/createASFClinicalTrials.sql", "r", encoding="utf-8") as file:
         sql_commands = file.read()
-    execRequest(sql_commands)
-    
+    __execRequest(sql_commands)
     print("--- Scraper")
-    #2) Use scraper to obtain dataframe
+    # 2) Use scraper to obtain dataframe
     with open(f"{wkdir}/../data/asf_clinics.json") as f:
         clinics_json = json.load(f)
     clinics_json_df = pd.read_json(f"{wkdir}/../data/asf_clinics.json", orient="index")
-    df =  scrASFClinicalTrial.trials_asf_clinics(clinics_json_df,clinics_json)
-    df  = df.replace([np.inf, -np.inf], np.nan)
-    df.fillna("None",inplace = True)
+    df = scrASFClinicalTrial.trials_asf_clinics(clinics_json_df, clinics_json)
+    df = df.replace([np.inf, -np.inf], np.nan)
+    df.fillna("None", inplace=True)
 
-    #3) Insert value in Table from dataframe
-    insertValue(df,"T_ASFClinicalTrials")
-    print("Execute time for ASFClinicalTrials : ",round(time.time() - start,2), "s")
+    # 3) Insert value in Table from dataframe
+    __insertValue(df, "T_ASFClinicalTrials")
+    print("Execute time for ASFClinicalTrials : ", round(time.time()-start, 2), "s")
+
 
 if __name__ == "__main__":
+    """
+    Endpoint to launch the different scrapers with injection of the results into the database 
+    """
     start = time.time()
-    
     articlesPubMed()
     print("\n")
     asTrials()
@@ -220,4 +272,4 @@ if __name__ == "__main__":
     unPopulation()
     print("\n")
     asfClinicalTrials()
-    print("\nExecute time : ",round(time.time() - start,2), "s")
+    print("\nExecute time : ", round(time.time()-start, 2), "s")
